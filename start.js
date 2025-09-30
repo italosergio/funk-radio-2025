46d33e2b891c21bb9978042500ef302dc6980bb5
Ítalo Sérgio <italosergio@mail.com>
2025-09-30 19:55:19 -0300

    fix: converte start.js para ES modules

--- start.js
+++ start.js
@@ -1,5 +1,8 @@
-const { spawn } = require('child_process')
-const path = require('path')
+import { spawn } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // Inicia o servidor
 const server = spawn('node', ['server.js'], {

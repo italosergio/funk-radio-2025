4f2295a57d61ea92dcd15a170d57e03586106a07
Ítalo Sérgio <italosergio@mail.com>
2025-09-30 19:57:11 -0300

    fix: corrige __dirname no servidor para ES modules

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 
